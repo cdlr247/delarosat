a70c04b82ce403e6c380fe69f06babf2938db435
cdlr247 <cdlr01@hotmail.com>
2020-10-02 13:22:02 -0500

    proyecto tareas

---
+++ DelaRosaT/src/seriefiboswing/FibonacciSwing.java
@@ -0,0 +1,56 @@
+package seriefiboswing;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class FibonacciSwing implements ActionListener{
+	
+	JFrame frame;
+	JPanel pnl;
+	JTextField tf;
+	JLabel lbl;
+	JButton btn;
+	
+	public FibonacciSwing()
+	{
+		frame = new JFrame("Serie fibonacci");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(500, 150);
+		
+		pnl = new JPanel();
+		tf = new JTextField(10);
+		btn = new JButton("Calcular");
+		btn.addActionListener(this);
+		lbl = new JLabel("Introduzca el número de iteraciones");
+		pnl.add(lbl);
+		pnl.add(tf);
+		pnl.add(btn);
+		frame.add(pnl);
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+		
+	}
+
+	public static void main(String[] args) {	
+		FibonacciSwing fibo = new FibonacciSwing();
+	}
+	
+	public void actionPerformed(ActionEvent e)
+	{
+		if (e.getSource()== btn)
+		{
+			int n1 = 0, n2 = 1, n3 = 0, lim, i = 1;
+			String resultado = Integer.toString(n1) + " " + Integer.toString(n2);
+			lim = Integer.parseInt(tf.getText());
+			do
+	        {
+	            n3 = n1 + n2;
+	            resultado = resultado + " " + Integer.toString(n3);
+	            n1 = n2; n2 = n3;
+	            i++;
+	        } while(i<=lim);
+			JOptionPane.showMessageDialog(null, resultado);
+		}
+	}
+
+}
